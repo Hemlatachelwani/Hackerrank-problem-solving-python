f9e61e997f26c502b1e9c23eb1c6af6a6082aa93
Hemlata Chelwani <30999169+Hemlatachelwani@users.noreply.github.com>
2021-01-20 17:43:32 +0530

    Create birthday-cake-candles.py

---
+++ Problem :     You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.  Example   The maximum height candles are  units high. There are  of them, so return .  Function Description  Complete the function birthdayCakeCandles in the editor below.  birthdayCakeCandles has the following parameter(s):  int candles[n]: the candle heights      *********************************** Solution:   #!/bin/Birthday-Cake-Candles/birthday-cake-candles.py
@@ -0,0 +1,41 @@
+Problem :
+    You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Example
+
+
+The maximum height candles are  units high. There are  of them, so return .
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+
+birthdayCakeCandles has the following parameter(s):
+
+int candles[n]: the candle heights
+
+    ***********************************
+Solution:
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    tall=max(candles)
+    res=candles.count(tall)
+    return res
+    
