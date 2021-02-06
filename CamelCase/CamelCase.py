a55cc09727cf4d0dc855876a2f998e000cb8537b
Hemlata Chelwani <30999169+Hemlatachelwani@users.noreply.github.com>
2021-02-06 09:15:39 +0530

    Create CamelCase.py

---
+++ CamelCase/CamelCase.py
@@ -0,0 +1,40 @@
+There is a sequence of words in CamelCase as a string of letters, , having the following properties:
+
+It is a concatenation of one or more words consisting of English letters.
+All letters in the first word are lowercase.
+For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+Given , determine the number of words in .
+
+Example
+
+There are  words in the string: 'one', 'Two', 'Three'.
+
+Solution:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the camelcase function below.
+def camelcase(s):
+    cnt=1
+    for chartr in s:
+      if chartr.isupper():
+        cnt+=1
+        print(cnt)
+        
+    return(cnt)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
