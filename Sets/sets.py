8a41b4badfd4fc47e1d7857fce5c14d46b2785a4
Hemlata Chelwani <30999169+Hemlatachelwani@users.noreply.github.com>
2021-02-10 18:31:07 +0530

    Create sets.py

---
+++ Sets/sets.py
@@ -0,0 +1,27 @@
+Task
+
+Now, let's use our knowledge of sets and help Mickey.
+
+Ms. Gabriel Williams is a botany professor at District College. One day, she asked her student Mickey to compute the average of all the plants with distinct heights in her greenhouse.
+
+Formula used:
+
+average = Sum of total disctinct heights /total number of distinct heights
+
+Solution:
+
+def average(array):
+    # your code goes here
+    sum1=0
+    set_arr=set(arr)
+    #print(set_arr)
+    for i in set_arr:
+        
+        sum1+=i
+    return(sum1/len(set_arr))
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
